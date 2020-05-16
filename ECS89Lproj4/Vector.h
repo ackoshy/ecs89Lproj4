2d93c97a83892838242e2969df80c0fa84c72981
ackoshy <59678079+ackoshy@users.noreply.github.com>
2020-05-16 14:41:53 -0700

    Add files via upload

---
+++ ECS89Lproj4/Vector.h
@@ -0,0 +1,34 @@
+#ifndef VECTOR_H 	 	    		
+#define VECTOR_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+typedef struct STRUCT_VECTOR_TAG SVector;
+typedef SVector *SVectorRef;
+
+typedef int (*TVectorElementCompare)(const void *, const void *);
+
+SVectorRef VectorCreate(size_t elemsize);
+
+bool VectorDestroy(SVectorRef vector);
+
+size_t VectorSize(SVectorRef vector);
+
+void *VectorElement(SVectorRef vector, size_t index);
+
+#define VectorElementAs(vect,idx,type)      ((type *)VectorElement((vect),(idx)))
+
+bool VectorElementAppend(SVectorRef vector, void *elem);
+
+void VectorSort(SVectorRef vector, TVectorElementCompare compare);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
